5cbf8cce4c16b11c6a63e08af958db25c652f2e1
Divij Mahajan <d.mahajan2004@gmail.com>
2025-05-14 04:15:18 +0530

    fixed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import SideNav from './components/Sidenav';
 import './index.css'
-import SideNav from '../components/Sidenav'
+
 
 function App() {
   return (
     <div className="flex h-screen">
-      <SideNav />
-      <main className="flex-1 overflow-auto bg-white p-4">
+        <SideNav/>
+      <main className="flex-1 overflow-auto bg-red-800 p-4">
         {/* Your main content here */}
       </main>
       
