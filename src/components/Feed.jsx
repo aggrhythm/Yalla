c105d0d5ed4e816c23e65ea87865e3bb2b2f42ca
Divij Mahajan <d.mahajan2004@gmail.com>
2025-05-24 03:51:37 +0530

    images update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,32 +3,32 @@ import Masonry from 'react-masonry-css';
 const memes = [
   {
     id: 1,
-    imageUrl: "/src/assets/Meme1.jpeg",
+    imageUrl: "/Meme1.jpeg",
     alt: "Game of Thrones meme",
   },
   {
     id: 2,
-    imageUrl: "/src/assets/Meme2.jpeg",
+    imageUrl: "/Meme2.jpeg",
     alt: "Emotional damage",
   },
   {
     id: 3,
-    imageUrl: "/src/assets/Meme3.jpeg",
+    imageUrl: "/Meme3.jpeg",
     alt: "No stress cat",
   },
   {
     id: 4,
-    imageUrl: "/src/assets/Meme4.jpeg",
+    imageUrl: "/Meme4.jpeg",
     alt: "Marketing agency",
   },
   {
     id: 5,
-    imageUrl: "/src/assets/Meme5.jpeg",
+    imageUrl: "/Meme5.jpeg",
     alt: "Distracted boyfriend meme",
   },
   {
     id: 6,
-    imageUrl: "/src/assets/Meme6.jpeg",
+    imageUrl: "/Meme6.jpeg",
     alt: "Pepe crying",
   },
 ];
